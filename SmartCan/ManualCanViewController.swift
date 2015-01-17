b966b7fbde22193f6bdac6b5c5b670cd799b4132
Colin <colin@seas.1007.apng.wireless-pennnet.upenn.edu>
2015-01-17 04:33:23 -0500

    Proper UIFont implementation and Can operation buttons

--- SmartCan/ManualCanViewController.swift
+++ SmartCan/ManualCanViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ManualCanViewController: UIViewController {
+class ManualCanViewController: UINavigationController {
     
     var titleLabel: UILabel = UILabel(frame: CGRectMake(50, 40, 200, 100))
     
@@ -18,9 +18,23 @@ class ManualCanViewController: UIViewController {
         println("Welcome to the Manual Can View Controller")
         
         self.view.backgroundColor = UIColor.whiteColor()
-        titleLabel.font = UIFont(name: UILabel.appearance().font.familyName, size: 23)
-        titleLabel.text = "Manual Can"
         self.view.addSubview(titleLabel)
         
+        
+        
+        // Add Trash Button
+        let tframe = CGRectMake(20, 150, 340, 200)
+        let tButton = HTPressableButton(frame: tframe, buttonStyle: HTPressableButtonStyle.Rect)
+        tButton.buttonColor = UIColor.ht_grapeFruitColor()
+        tButton.shadowColor = UIColor.ht_grapeFruitDarkColor()
+        tButton.setTitle("TRASH", forState: UIControlState.Normal)
+        self.view.addSubview(tButton)
+        // Add Recycling Button
+        let rframe = CGRectMake(20, 550, 340, 200)
+        let rButton = HTPressableButton(frame: rframe, buttonStyle: HTPressableButtonStyle.Rect)
+        rButton.buttonColor = UIColor.ht_mintColor()
+        rButton.shadowColor = UIColor.ht_mintDarkColor()
+        rButton.setTitle("RECYCLE", forState: UIControlState.Normal)
+        self.view.addSubview(rButton)
     }
 }
